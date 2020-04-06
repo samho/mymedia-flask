28e0cdf88d3e04310888de162b2de8a8f7874265
Sam Ho <samhocngz@gmail.com>
2020-04-06 16:24:02 +0800

    change the mode for deleting record, and create the new and create page of user.

--- applications/users/forms.py
+++ applications/users/forms.py
@@ -1,10 +1,32 @@
-from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired, Length
+from applications.utils import dbmanager
 
 
-class UserForm(Form):
-
+class UserForm(FlaskForm):
     username = StringField("Your user name.", validators=[DataRequired(), Length(max=255)])
-    password = PasswordField("Your user name.", validators=[DataRequired()])
-    submit = SubmitField("Submit")
+    password = PasswordField("Your password.", validators=[DataRequired()])
+
+    def validate(self):
+        super(UserForm, self).validate()
+
+        if self.username.data.strip() == "":
+            self.username.errors.append("User name can not be empty.")
+            return False
+
+        if self.password.data.strip() == "":
+            self.password.errors.append("Passowrd can not be empty.")
+            return False
+
+        user = dbmanager.find_user_by_name(username=self.username.data)
+
+        if not user:
+            dbmanager.save_user(self.username.data, self.password.data)
+            return True
+        else:
+            self.username.errors.append("User is existed.")
+            return False
+
+
+
--- applications/users/views.py
+++ applications/users/views.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, flash
 from applications.main.forms import LoginForm
+from applications.users.forms import UserForm
 from applications.utils import dbmanager
 
 users = Blueprint("users",
@@ -20,4 +21,41 @@ def user_index():
             return render_template("users.html", pagename="Users", logon_user=session['username'])
         else:
             return render_template("users.html", pagename="Users", logon_user=session['username'], users=user_list)
+
+
+@users.route('/delete_confirm/<int:user_id>')
+def delete_confirm(user_id):
+    if 'username' not in session:
+        return render_template('login.html', form=LoginForm())
+
+    user = dbmanager.find_user_by_id(user_id)
+    if user is None:
+        return render_template('delete_confirm.html', pagename="User Delete Confirm", logon_user=session['username'])
+    else:
+        return render_template('delete_confirm.html', pagename="User Delete Confirm", logon_user=session['username'], user=user)
+
+
+@users.route('/new')
+def new_user():
+    if 'username' not in session:
+        return render_template('login.html', form=LoginForm())
+
+    return render_template('/user_new.html', logon_user=session['username'], userform=UserForm())
+
+
+@users.route('/create_user', methods=('GET', 'POST'))
+def create_user():
+    if 'username' not in session:
+        return render_template('login.html', form=LoginForm())
+
+    userform = UserForm()
+    if userform.validate_on_submit():
+        user_list = dbmanager.find_all_users()
+        if user_list is None:
+            return render_template("users.html", pagename="Users", logon_user=session['username'])
+        else:
+            return render_template("users.html", pagename="Users", logon_user=session['username'], users=user_list)
+
+    return render_template("/user_new.html", userform=userform)
+
 
